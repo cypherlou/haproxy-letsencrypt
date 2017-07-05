c73e3976e576d6225d3cf15319ce2c904766a010
Glenn <cypherlou666@gmail.com>
2017-07-05 10:20:02 +0100

    added flag to turn colour logging off

--- letsencrypt-config.sh
+++ letsencrypt-config.sh
@@ -1,12 +1,8 @@
 # general purpose definitions - mostly from pretty logging
-RESET="\e[39m"
-RED="\e[31m"
-LRED="\e[91m"
-YELLOW="\e[33m"
-BLUE="\e[94m"
 
 # config used by letsencrypt-new.sh
 # post to start letsencrypt on. Note this needs to match up with the HAProxy configuration you have used.
+COLOUR_LOGGING=0
 BASE_PORT=9999
 LETS_ENCRYPT_DIR="/usr/local/bin/letsencrypt"
 CERT_CMD="${LETS_ENCRYPT_DIR}/letsencrypt-auto certonly --standalone --preferred-challenges http --http-01-port ${BASE_PORT} --renew-by-default --agree-tos --quiet --non-interactive"
@@ -23,3 +19,11 @@ OPEN_SSL_CMD="/usr/bin/openssl"
 
 # config used by
 CERT_RENEW_CMD="${LETS_ENCRYPT_DIR}/letsencrypt-auto renew --standalone --preferred-challenges http --http-01-port ${BASE_PORT} --agree-tos --non-interactive"
+
+if [[ "$COLOUR_LOGGING" -eq 1 ]]; then
+    RESET="\e[39m"
+    RED="\e[31m"
+    LRED="\e[91m"
+    YELLOW="\e[33m"
+    BLUE="\e[94m"
+fi
